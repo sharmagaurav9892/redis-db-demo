60526f0062c8f12630d4f085040c64eed7feba61
gauravsharma <gaurav.sharma@rksv.in>
2023-06-27 22:00:28 +0530

    Initial Commit.

---
+++ src/main/java/xyz/gauravsharma/redisdbdemo/RedisDbDemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.gauravsharma.redisdbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/gauravsharma/redisdbdemo/RedisDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gauravsharma.redisdbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
